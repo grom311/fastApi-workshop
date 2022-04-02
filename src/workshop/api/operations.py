4635503b8c99bc4f4b93a7f11037a62d9a7e6991
Sergei <50500581+grom311@users.noreply.github.com>
2022-04-02 23:36:08 +0300

    jwt authenticate (#2)
    fix

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,35 +1,49 @@
 from typing import List, Optional
-from webbrowser import Opera
 from fastapi import APIRouter, Response, status
 from models.operations import OperationUpdate, Operation, OperationKind, OperationCreate
 from fastapi import Depends
 
 from services.operations import OperationsService
+from services.auth import get_current_user
+from tables import User
 
 router = APIRouter(
     prefix='/operations',
 )
 
 @router.get('/', response_model=List[Operation])
-def get_operations(kind: Optional[OperationKind] = None, service: OperationsService = Depends()):
-    return service.get_list(kind=kind)
+def get_operations(
+    kind: Optional[OperationKind] = None, service: OperationsService = Depends(),
+    user: User = Depends(get_current_user),
+    ):
+    return service.get_list(user_id=user.id, kind=kind)
     
 @router.post('/', response_model=Operation)
 def create_operation(
     operation_data: OperationCreate,
+    user: User = Depends(get_current_user),
     service: OperationsService = Depends()):
-    return service.create(operation_data)
+    return service.create(user_id=user.id, operation_data=operation_data)
 
 @router.get('/{operation_id}', response_model=Operation)
-def get_operation_id(operation_id: int, service: OperationsService = Depends()):
-    return service.get(operation_id)
+def get_operation_id(
+    operation_id: int,
+    user: User = Depends(get_current_user),
+    service: OperationsService = Depends()):
+    return service.get(user_id=user.id, operation_id=operation_id)
 
 @router.put('/{operation_id}', response_model=Operation)
-def update_operation(operation_id: int, operation_data: OperationUpdate,
-service: OperationsService = Depends()):
-    return service.update(operation_id, operation_data)
+def update_operation(
+    operation_id: int,
+    operation_data: OperationUpdate,
+    user: User = Depends(get_current_user),
+    service: OperationsService = Depends()):
+    return service.update(user_id=user.id, operation_id=operation_id, operation_data=operation_data)
 
 @router.delete('/{operation_id}')
-def delete_operation(operation_id: int, service: OperationsService = Depends()):
-    service.delete(operation_id)
+def delete_operation(
+    operation_id: int,
+    user: User = Depends(get_current_user),
+    service: OperationsService = Depends()):
+    service.delete(user_id=user.id, operation_id=operation_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -4,6 +4,10 @@ class Settings(BaseSettings):
     server_hort: str = '127.0.0.1'
     server_port: int = 8000
     database_url = "sqlite:///./database.sqlite3"
+    jwt_secret : str
+    jwt_algoritm: str = 'HS256'
+    jwt_expire: int = 3600
+
 
 settings = Settings(
     _env_file='.env',
