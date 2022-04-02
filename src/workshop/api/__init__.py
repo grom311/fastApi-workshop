04f186bd75036fd0f4ed604232d3013558580c6b
grom311 <grom311@yandex.ru>
2022-04-02 23:34:45 +0300

    jwt authenticate

--- src/workshop/api/__init__.py
+++ src/workshop/api/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from .operations import router as op_router
+from .auth import router as auth_router
+
 
 router = APIRouter()
 router.include_router(op_router)
+router.include_router(auth_router)
