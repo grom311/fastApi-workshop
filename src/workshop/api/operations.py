2d3498b3d44f2232acf168400a7ea0b9520f37d5
Sergei <50500581+grom311@users.noreply.github.com>
2022-04-02 12:51:31 +0300

    update, delete, get (#1)
    fixed

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,6 +1,7 @@
-from typing import List
-from fastapi import APIRouter
-from models.operations import Operation
+from typing import List, Optional
+from webbrowser import Opera
+from fastapi import APIRouter, Response, status
+from models.operations import OperationUpdate, Operation, OperationKind, OperationCreate
 from fastapi import Depends
 
 from services.operations import OperationsService
@@ -10,6 +11,25 @@ router = APIRouter(
 )
 
 @router.get('/', response_model=List[Operation])
-def get_operations(service: OperationsService = Depends()):
-    return service.get_list()
+def get_operations(kind: Optional[OperationKind] = None, service: OperationsService = Depends()):
+    return service.get_list(kind=kind)
     
+@router.post('/', response_model=Operation)
+def create_operation(
+    operation_data: OperationCreate,
+    service: OperationsService = Depends()):
+    return service.create(operation_data)
+
+@router.get('/{operation_id}', response_model=Operation)
+def get_operation_id(operation_id: int, service: OperationsService = Depends()):
+    return service.get(operation_id)
+
+@router.put('/{operation_id}', response_model=Operation)
+def update_operation(operation_id: int, operation_data: OperationUpdate,
+service: OperationsService = Depends()):
+    return service.update(operation_id, operation_data)
+
+@router.delete('/{operation_id}')
+def delete_operation(operation_id: int, service: OperationsService = Depends()):
+    service.delete(operation_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -1,17 +1,47 @@
-from typing import List
-from fastapi import Depends
+from typing import List, Optional
+from fastapi import Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from database import get_session
 import tables
+from models.operations import OperationUpdate, OperationKind, OperationCreate
+
 
 class OperationsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get_list(self)-> List[tables.Operation]:
-        operations = (
-            self.session.query(tables.Operation)
-            .all()
-        )
+    def _get(self, operation_id: int) -> tables.Operation:
+        operation = (self.session.query(tables.Operation)
+        .filter_by(id=operation_id)
+        .first())
+        if not operation:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return operation
+        
+    def get_list(self, kind: Optional[OperationKind] = None)-> List[tables.Operation]:
+        query = self.session.query(tables.Operation)
+        if kind:
+            query = query.filter_by(kind=kind)
+        operations = query.all()
         return operations
+        
+    def create(self, operation_data: OperationCreate) -> tables.Operation:
+        operration = tables.Operation(**operation_data.dict())
+        self.session.add(operration)
+        self.session.commit()
+        return operration
+
+    def get(self, operation_id: int) -> tables.Operation:
+        return self._get(operation_id)
+
+    def update(self,operation_id: int,  operation_data: OperationUpdate) -> tables.Operation:
+        operation = self._get(operation_id)
+        for field, value in operation_data:
+            setattr(operation, field, value)
+        self.session.commit()
+        return operation
+
+    def delete(self, operation_id: int):
+        operation = self._get(operation_id)
+        self.session.delete(operation)
         
